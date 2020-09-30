50a729515b5dad97050484e7196ced99a9170a72
VChouliak <chouliak@hotmail.com>
2020-09-30 18:28:54 +0200

    Implement yearDifference method

--- src/Service/DateCalculator.php
+++ src/Service/DateCalculator.php
@@ -6,5 +6,11 @@ namespace App\Service;
 
 class DateCalculator
 {
+    public function yearsDifference($year){
+        $curYear = date('Y');
+        $diff = $curYear - $year;
+
+        return $diff;
+    }
 
 }
