6dc8dd3f8008deadf2bd0d947e7049bb36f07527
VChouliak <chouliak@hotmail.com>
2020-10-01 12:18:44 +0200

    Add field "hotel" to home-action

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,7 +3,7 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Hotel;
 use App\Service\DateCalculator;
 use App\Service\RandomNumberGenerator;
 use Psr\Log\LoggerInterface;
@@ -15,7 +15,9 @@ class IndexController extends AbstractController
     private const HOTEL_OPENED = 1969;
     public function home(LoggerInterface $logger, DateCalculator $dateCalculator){
         $logger -> info('Homepage loaded');
-
+        $hotels = $this->getDoctrine()
+            ->getRepository(Hotel::class)
+            ->findAll();
         $year = $dateCalculator->yearsDifference(self::HOTEL_OPENED);;
         $images = [
             ['url' => 'images/hotel/intro_room.jpg', 'class' => ''],
@@ -24,6 +26,6 @@ class IndexController extends AbstractController
             ['url' => 'images/hotel/intro_attractions.jpg', 'class' => ''],
             ['url' => 'images/hotel/intro_wedding.jpg', 'class' => 'hidesm'],
         ];
-        return $this->render('index.html.twig', ['year' =>$year, 'images' => $images]);
+        return $this->render('index.html.twig', ['year' =>$year, 'images' => $images, 'hotels' => $hotels]);
     }
 }
