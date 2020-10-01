d0e7b136d73ce0b8a8b9e2a1faea9f7846926702
VChouliak <chouliak@hotmail.com>
2020-10-01 13:27:28 +0200

    Set hardcoded tokens and implement beforController - method

--- src/EventSubscriber/TokenSubscriber.php
+++ src/EventSubscriber/TokenSubscriber.php
@@ -4,14 +4,32 @@
 namespace App\EventSubscriber;
 
 
+use App\Controller\ApiController;
+use App\Controller\TokenAuthenticatedController;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class TokenSubscriber implements EventSubscriberInterface
 {
+    private $tokens;
 
-    public function beforeController(){
+    public function __construct(){
+        $this->tokens = [
+            'user1' => 'token1',
+            'user2' => 'token2'
+        ];
+    }
 
+    public function beforeController(ControllerEvent $event){
+        $controller = $event->getController();
+        if(is_array($controller) && $controller[0] instanceof TokenAuthenticatedController){
+            $token = $event->getRequest()->query->get('token');
+            if(!in_array($token,$this->tokens)){
+                throw new AccessDeniedHttpException('Invalid token');
+            }
+        }
     }
 
     public static function getSubscribedEvents()
